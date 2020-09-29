aa14b153b8cd912df266f6ca9a7b8b91e439f0ee
achakrabor24 <71959953+achakrabor24@users.noreply.github.com>
2020-09-29 15:02:20 -0500

    Add files via upload

---
+++ TestClassApp.java
@@ -0,0 +1,212 @@
+// --== CS400 File Header Information ==--
+// Name: Aritra Chakraborty
+// Email: achakrabor24@wisc.edu
+// Team: FD
+// TA: Abhay
+// Lecturer: Dahl
+// Notes to Grader: <optional extra notes>
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+/**
+ * Tests all functions of app.
+ */
+public class TestClassApp {
+  // Data Wranglers testing
+    /**
+     * Checks if class information like GPA, percentage of As, and percentage
+     * of Fs is properly initialized in the class.
+     * @return
+     */
+  public static boolean testConstructor(){ // ECE210 3.97 96.6 0.1
+    AcademicClass ECE210 = new AcademicClass("ECE210",3.97, 96.6, .1);
+    if(ECE210.getAvgGPA() != 3.97) {
+      return false;
+    }
+    if(ECE210.getAvgGPA() != 3.97) {
+      return false;
+    }
+    if(ECE210.getPercentA() != 96.6) {
+      return false;
+    }
+    if(ECE210.percentF() != .1) {
+      return false;
+    }
+
+    return true;
+  }
+
+
+  /**
+   * Checks two cases: if file exists and if file does not exist.
+   * @return true if working, false otherwise
+   */
+  public static boolean testReadFile(){
+    // If file does not exist
+    try {
+      FrontEnd.dataWrangler("UWClass.txt");
+    } catch (FileNotFoundException e1){
+      if(e1.getMessage() == null || !e1.getMessage().equals("File not found!")){
+        System.out.println("    FileNotFoundException not working.");
+        return false;
+      }
+    } catch (Exception e2){
+      System.out.println("    Another Exception not working.");
+      return false;
+    }
+
+    // If file does exist
+    try {
+      FrontEnd.dataWrangler("UWClasses.txt");
+    } catch (FileNotFoundException e1){
+      if(e1.getMessage() == null || !e1.getMessage().equals("File not found!")){
+        System.out.println("    FileNotFoundException not working.");
+        return false;
+      }
+    } catch (Exception e2){
+      System.out.println("    Another Exception not working." + e2.getMessage());
+
+      return false;
+    }
+
+    return true;
+  }
+
+
+  // FrontEnd testing
+
+  /**
+   * Checks if method correctly identifies a class key.
+   * @return
+   */
+  public static boolean testContainsKey(){
+    try {
+      FrontEnd.dataWrangler("UWClasses.txt");
+    } catch (FileNotFoundException e){
+      System.out.println("File not found!");
+    }
+
+    if(!FrontEnd.getMap().containsKey("ECE210")){
+      return false;
+    }
+
+    if(FrontEnd.getMap().containsKey("CS100")){
+      return false;
+    }
+
+    return true;
+  }
+
+  // BackEnd testing
+  /**
+   * Checks if size is correct.
+   * @return
+   */
+  public static boolean testSize(){
+    BackEnd classes = new BackEnd();
+    classes.put("CS200", new AcademicClass("CS200", 2.5, 90, 10));
+    classes.put("CS100", new AcademicClass("CS100", 2.5, 90, 10));
+    classes.put("CS300", new AcademicClass("CS300", 2.5, 90, 10));
+    classes.put("CS354", new AcademicClass("CS354", 2.5, 90, 10));
+
+    if(classes.size() != 4){
+      System.out.println(classes.size());
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * Tests if the put method correctly inserts keys and values.
+   * @return
+   */
+  public static boolean testPut() {
+    BackEnd classes = new BackEnd();
+    classes.put("CS200", new AcademicClass("CS200", 2.5, 90, 10));
+
+    if(!classes.containsKey("CS200")){
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * Checks if method properly retrieves class information given class key.
+   * @return
+   */
+  public static boolean testGet(){
+    BackEnd classes = new BackEnd();
+    classes.put("CS200", new AcademicClass("CS200", 2.5, 90, 10));
+    classes.put("CS100", new AcademicClass("CS100", 2.5, 90, 10));
+    classes.put("CS300", new AcademicClass("CS300", 2.5, 90, 10));
+    classes.put("CS354", new AcademicClass("CS354", 2.5, 90, 10));
+
+    AcademicClass CS200 = classes.get("CS200");
+
+    if(!CS200.getClassName().equals("Cs200") && CS200.getAvgGPA() != 2.5 && CS200.getPercentA() != 90
+    && CS200.percentF() != 10){
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * Checks if a class key and contents is successfully removed.
+   * @return
+   */
+  public static boolean testRemove(){
+    BackEnd classes = new BackEnd();
+    classes.put("CS200", new AcademicClass("CS200", 2.5, 90, 10));
+    classes.put("CS100", new AcademicClass("CS100", 2.5, 90, 10));
+    classes.put("CS300", new AcademicClass("CS300", 2.5, 90, 10));
+    classes.put("CS354", new AcademicClass("CS354", 2.5, 90, 10));
+
+    classes.remove("CS200");
+
+    if(classes.containsKey("CS200") && classes.size() != 3){
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * Tests if all class keys and contents are cleared.
+   * @return
+   */
+  public static boolean testClear(){
+    BackEnd classes = new BackEnd();
+    classes.put("CS200", new AcademicClass("CS200", 2.5, 90, 10));
+    classes.put("CS100", new AcademicClass("CS100", 2.5, 90, 10));
+    classes.put("CS300", new AcademicClass("CS300", 2.5, 90, 10));
+    classes.put("CS354", new AcademicClass("CS354", 2.5, 90, 10));
+
+    classes.clear();
+
+    if(classes.size() != 0){
+      return false;
+    }
+
+    return true;
+  }
+
+  public static void main(String args[]){
+    System.out.println("testConstructor(): "+ testConstructor());
+    System.out.println("testContainsKey(): "+ testContainsKey());
+    System.out.println("testSize(: "+ testSize());
+    System.out.println("testPut(): "+ testPut());
+    System.out.println("testGet(): "+ testGet());
+    System.out.println("testRemove(): "+ testRemove());
+    System.out.println("testClear(): "+ testClear());
+    System.out.println("testReadFile(): " + testReadFile());
+
+  }
+
+}
